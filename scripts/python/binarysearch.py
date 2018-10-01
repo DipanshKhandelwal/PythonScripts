f7934f68a929f9e8e9ab1918843cccd6295a1c5e
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-10-01 20:40:48 +0530

    added binary search

---
+++ scripts/python/binarysearch.py
@@ -0,0 +1,32 @@
+def binarySearch(arr, l, r, x): 
+  
+    while l <= r: 
+  
+        mid = l + (r - l)/2; 
+          
+      
+        if arr[mid] == x: 
+            return mid 
+  
+        
+        elif arr[mid] < x: 
+            l = mid + 1
+  
+      
+        else: 
+            r = mid - 1
+      
+    return -1
+  
+  
+
+arr = [ 2, 3, 4, 10, 50 ] 
+x = 50
+  
+# Function call 
+result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print "Element is present at index %d" % result 
+else: 
+    print "Element is not present in array"
