de92c7711162e77ffb71a29e8797d624cc31f565
Ahmad Javed <ahmadjaved97@gmail.com>
2018-10-01 17:56:03 +0530

    Added merge_sort.py

---
+++ scripts/python/merge_sort.py
@@ -0,0 +1,39 @@
+def mergeSort(alist):
+	
+
+	if len(alist) > 1:
+		mid = len(alist) // 2
+		lefthalf = alist[:mid]
+		righthalf = alist[mid:]
+
+		mergeSort(lefthalf)
+		mergeSort(righthalf)
+
+		i,j,k = 0,0,0
+
+		while i < len(lefthalf) and j < len(righthalf):
+			if lefthalf[i] < righthalf[j]:
+				alist[k] = lefthalf[i]
+				i = i + 1
+			else:
+				alist[k] = righthalf[j]
+				j = j + 1
+			k = k + 1
+
+		while i < len(lefthalf):
+			alist[k] = lefthalf[i]
+			i = i + 1
+			k = k + 1
+
+		while j < len(righthalf):
+			alist[k] = righthalf[j]
+			j = j + 1
+			k = k + 1
+	
+
+
+print('Enter the numbers to be sorted (using space):')
+alist = list(map(int, input().split()))
+mergeSort(alist)
+for i in range(len(alist)):
+	print(alist[i], end = ' ')
