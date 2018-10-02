e9469338b8fec26c1a72fd99a2829a19b6d315d8
Pulkit <khuranapulkit.pk@gmail.com>
2018-10-02 14:54:38 +0530

    Check Prime or Not Optimised"

--- scripts/python/CheckPrimeorNot.py
+++ scripts/python/CheckPrimeorNot.py
@@ -1,9 +1,18 @@
-a=int(input("Enter number: "))
-k=0
-for i in range(2,a//2+1):
-    if(a%i==0):
-        k=k+1
-if(k<=0):
-    print("Number is prime")
+''' This Algo is based on that every number other then
+2 and 3 is of the form 6*k+1 and 6*k-1'''
+def prime(x):
+    for i in range(2,x//2+1):
+        if x%i==0:
+            return 0
+    return 1
+n=int(input())
+if n==2 or n==3:
+    print ("Prime")
+elif (n-1)%6==0 or (n+1)%6==0:
+    if prime(n):
+        print("Prime")
+    else:
+        print("Not Prime")
 else:
-    print("Number isn't prime")
+    print("Not Prime")
+    
