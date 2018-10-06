02170e5f21c29d690ad973b7fbcab221b1072d61
Brent Matos <43549958+matosBrent@users.noreply.github.com>
2018-10-07 01:44:23 +0530

    Updated to DRY

--- scripts/python/scheduler.py
+++ scripts/python/scheduler.py
@@ -3,6 +3,7 @@
     import webbrowser
     import time
     def main():
+        path = "Prod.txt"
         print ("Welcome Harvey !!!! :) \nThe Scheduler has Started and is ACTIVE"
         while(1):
             tempTime = str(datetime.datetime.now().time())
@@ -18,49 +19,49 @@
             sec = tempSecString[0:2]
             if( minute == '24' and sec == '01'):
                 if hour == '11':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Please send in the 11pm - 12am CST report")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '12':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Please update the Client about the latest Web Server Report\n")
-                    webbrowser.open('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open(path)
                     time.sleep(1)
                 elif hour == '13':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Please send in the 1am - 2am CST report and also update the TGT daily Stats report. The purge process starts at 1330hrs IST\n")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '14':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Login via VPN and check the logs for any errors")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '15':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Run the Test for testing Load")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '16':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Configure DNS if not updated")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '17':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Please send in the 5am - 6am CST report")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '19':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Please send in the 7am - 8am CST report or ask Mr. X to take over the Report Work")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
                 elif hour == '10':
-                    with open('C:\Users\harvey\Desktop\Prod.txt','r+') as f:
+                    with open(path,'r+') as f:
                         f.write("Check Firewall settings")
-                    webbrowser.open_new_tab('C:\Users\harvey\Desktop\Prod.txt')
+                    webbrowser.open_new_tab(path)
                     time.sleep(1)
         print "My Scheduler stopped and is IN-ACTIVE"
     if __name__ == '__main__':
